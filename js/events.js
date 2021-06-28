011ef2ca8760b991777669575b9ad62882044292
Key Cuevas Melgarejo <38257705+KeyCuevasMelgarejo@users.noreply.github.com>
2021-06-28 11:40:25 -0500

    update
    remove textarea
    update keys width
    remove some events

--- js/events.js
+++ js/events.js
@@ -1,27 +1,22 @@
 const Events = {
     init() {        
         var alerta = document.getElementById('alert');
-        var textContent = document.getElementById('textcontent');
         var btnLimpiar = document.querySelector('.btn-limpiar');
         var textKeyboardInput = document.querySelector('.use-keyboard-input');
         var btnCloseAlert = document.getElementById('btn-close-alert');
 
         alerta.style="display:none";
 
-        // if you clic out of use-keyboard-input then close keyboard
-        textContent.onclick = function(e) {
-            e.preventDefault();
-            Keyboard.close();
-        }
-
         // you can't copy and paste from text
-        textContent.onpaste = function(e) {
+        textKeyboardInput.onpaste = function(e) {
             e.preventDefault();
+            textKeyboardInput.value = textKeyboardInput.value.substring(0, textKeyboardInput.value.length - 1);
             alerta.style="display:block";
             Keyboard.close();
         }
-        textContent.oncopy = function(e) {
+        textKeyboardInput.oncopy = function(e) {
             e.preventDefault();
+            textKeyboardInput.value = textKeyboardInput.value.substring(0, textKeyboardInput.value.length - 1);
             alerta.style="display:block";
             Keyboard.close();
         }
@@ -37,7 +32,6 @@ const Events = {
             e.preventDefault();
             textKeyboardInput.value="";
         }
-
     }
 };
 
