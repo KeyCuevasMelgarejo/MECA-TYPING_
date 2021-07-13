921aef539195c1f32f456539b2cfbbf7e8297ec0
Key Cuevas Melgarejo <38257705+KeyCuevasMelgarejo@users.noreply.github.com>
2021-07-12 22:37:19 -0500

    update
    finish 'manitos' events
    add animations
    add library
    modify some bugs
    
    Pendiente:
    Informe resultados de session

--- js/hands.js
+++ js/hands.js
@@ -1,17 +1,64 @@
-const Hands = {
-    init() {
-        document.querySelectorAll("circle").forEach(element=> {
-            this.fingerAnimation(element);
+const Hands = {  
+    fingers: {
+        properties: [
+            {name: "meniqueL", r: "5%", cx: "7.8mm", cy: "44mm"},
+            {name: "anularL", r: "6%", cx: "29mm", cy: "19mm"},
+            {name: "medioL", r: "6%", cx: "60mm", cy: "9mm"},
+            {name: "indiceL", r: "6%", cx: "93.5mm", cy: "17mm"},
+            {name: "gordoL", r: "5.5%", cx: "122.5mm", cy: "73.9mm"},
+            {name: "meniqueR", r: "5%", cx: "7.8mm", cy: "44mm"},
+            {name: "anularR", r: "6%", cx: "29mm", cy: "19mm"},
+            {name: "medioR", r: "6%", cx: "60mm", cy: "9mm"},
+            {name: "indiceR", r: "6%", cx: "93.5mm", cy: "17mm"},
+            {name: "gordoR", r: "5.5%", cx: "122.5mm", cy: "73.9mm"},
+        ]
+    },
+
+    fingerAnimation(fingerPending){
+        var element = document.querySelector(fingerPending);
+        var r = element.getAttribute("r");
+        var cx = element.getAttribute("cx");
+        var cy = element.getAttribute("cy");
+
+        this.fingers.properties.forEach(finger=>{
+            if(finger.name==element.className.animVal){
+                transition.begin(element, [
+                    {property: "r", from: r, to: finger.r},
+                    {property: "cx", from: cx, to: finger.cx},
+                    {property: "cy", from: cy, to: finger.cy},
+                    ["fill", "#E9E9E9", "#3CD2E6", "300ms"],
+                    ["stroke", "#5f5f5f", "#E9E9E9", "300ms"]
+                ], {
+                    // Both "property" transitions will use "linear" timing function
+                    timingFunction: "linear",
+                    // Only "property" will use "400ms" duration, the "fill" and "stroke" transitions define
+                    // their own duration of "300ms"
+                    duration: "300ms"
+                });
+            }
         });
     },
 
-    fingerAnimation(element){
-        transition.begin(element, [
-            ["transform", "translateY(0)", "translateY(0px)", "1s", "ease-in-out"],
-            ["fill", "#cf3e3e", "#E9E9E9", "500ms", "linear"],
-            ["stroke", "#cf3e3e", "#5f5f5f", "500ms", "linear"],
-            //["transform", "scale(1)", "scale(0.5)"]
-        ]);
+    fingerReturn(fingerPending){
+        var element = document.querySelector(fingerPending);
+        var r = element.getAttribute("r");
+        var cx = element.getAttribute("cx");
+        var cy = element.getAttribute("cy");
+
+        this.fingers.properties.forEach(finger=>{
+            if(finger.name==element.className.animVal){
+                transition.begin(element, [
+                    {property: "r", from: finger.r, to: r},
+                    {property: "cx", from: finger.cx, to: cx},
+                    {property: "cy", from: finger.cy, to: cy},
+                    ["fill", "#3CD2E6", "#E9E9E9"],
+                    ["stroke", "#E9E9E9", "#5f5f5f"]
+                ], {
+                    timingFunction: "linear",
+                    duration: "100ms"
+                });
+            }
+        });
     },
 
     open() {
@@ -26,7 +73,3 @@ const Hands = {
         });
     }
 };
-
-window.addEventListener("DOMContentLoaded", function () {
-    Hands.init();
-});
--- js/text.js
+++ js/text.js
@@ -1,7 +1,8 @@
 const Text = {
     init() {
         // var text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus culpa et quod reiciendis ullam sit aperiam libero laboriosam error distinctio repellendus omnis, quos perferendis facere sed! Minus fuga molestiae magni?";
-        var text = "Lorem\ni";
+        var text = "Lorem ipsum dolor sit\namet consectetur, adipisicing elit.\nNecessitatibus";
+        //var text="a b c d e f g h i j k l m n o p q r s t u v w y x z , ."
         // disable for copy and paste
         writeInput.readOnly = "true";
         placeHolderInput.readOnly= "true";
@@ -78,12 +79,18 @@ const Text = {
     removeAllText(placeHolderInput){
         var previousWord = placeHolderInput.innerHTML.charAt(0);
         var keyboardKey = document.querySelectorAll(".keyboard__key"); 
-        
+        var fingers = document.querySelectorAll("circle");
+
         this._specialWordsIndicateTop(previousWord);
 
         //remove all pendingColors
         keyboardKey.forEach(key => {
             key.classList.remove("keyboard__key--pending");
+        });
+
+        //remove all pendingColors finger
+        fingers.forEach(finger => {
+            Hands.fingerReturn("."+finger.className.animVal);
         });
 
         this._coloringPendingKey(previousWord);
@@ -93,12 +100,18 @@ const Text = {
         var writeInputLenght = writeInput.length;
         var previousWord = (placeHolderInput.innerHTML.charAt(writeInputLenght) && placeHolderInput.innerHTML[writeInputLenght].match(/\n/gm)!==null ? '\\n' :placeHolderInput.innerHTML.charAt(writeInputLenght));
         var keyboardKey = document.querySelectorAll(".keyboard__key"); 
+        var fingers = document.querySelectorAll("circle");
 
         this._specialWordsIndicateTop(previousWord);
         
-        //remove all pendingColors
+        //remove all pendingColors key
         keyboardKey.forEach(key => {
             key.classList.remove("keyboard__key--pending");
+        });
+
+        //remove all pendingColors finger
+        fingers.forEach(finger => {
+            Hands.fingerReturn("."+finger.className.animVal);
         });
 
         this._coloringPendingKey(previousWord);
@@ -111,18 +124,22 @@ const Text = {
             if(letra!=letra.toLowerCase() && key.innerText=="keyboard_arrow_up" && Keyboard.properties.capsLock==false){
                 key.classList.add("keyboard__key--pending");
                 Keyboard.properties.shiftPending=true;
+                Hands.fingerAnimation(".meniqueR");
             }
 
             // add color to the key which represents next word to type 
             if(key.innerText=="keyboard_return" && letra=='\\n'){
                 key.classList.add("keyboard__key--pending");
                 keyboardKey.length = 0;
+                Hands.fingerAnimation(".meniqueR");
             }else if(key.innerText=="space_bar" && letra==" "){
                 key.classList.add("keyboard__key--pending");
                 keyboardKey.length = 0;
+                Hands.fingerAnimation(".gordoR");
             }else if(key.innerText.toLowerCase()==letra.toLowerCase()){
                 key.classList.add("keyboard__key--pending");
                 keyboardKey.length = 0;
+                this._activeAnimation(letra.toLowerCase());
             }
         });
     },
@@ -131,21 +148,25 @@ const Text = {
         var keyboardKey = document.querySelectorAll(".keyboard__key");   
         keyboardKey.forEach(key => {
             // add color to 'shift' key when 'letra' is on LowerCase
-            if(key.innerText=="keyboard_arrow_up"){
+            if(letra==letra.toUpperCase() && key.innerText=="keyboard_arrow_up"){
                 key.classList.remove("keyboard__key--pending");
                 Keyboard.properties.shiftPending=false;
+                Hands.fingerReturn(".meniqueR");
             }
 
             // add color to the key which represents next word to type 
             if(key.innerText=="keyboard_return" && letra=='\\n'){
                 key.classList.remove("keyboard__key--pending");
                 keyboardKey.length = 0;
+                Hands.fingerReturn(".meniqueR");
             }else if(key.innerText=="space_bar" && letra==" "){
                 key.classList.remove("keyboard__key--pending");
                 keyboardKey.length = 0;
+                Hands.fingerReturn(".gordoR");
             }else if(key.innerText.toLowerCase()==letra.toLowerCase()){
                 key.classList.remove("keyboard__key--pending");
                 keyboardKey.length = 0;
+                this._inactiveAnimation(letra.toLowerCase());
             }
         });
     },
@@ -164,6 +185,76 @@ const Text = {
                 break;
             default:
                 cardText.innerHTML="La siguiente letra es: <p>"+word+"</p>";
+                break;
+        }
+    },
+
+    _activeAnimation(word){
+        switch (word) {
+            case "1": case "q": case "a": case "z": case "!":
+                Hands.fingerAnimation(".meniqueL");
+                break;
+            case "2": case "w": case "s": case "x":
+                Hands.fingerAnimation(".anularL");
+                break;
+            case "3": case "e": case "d": case "c":
+                Hands.fingerAnimation(".medioL");
+                break;
+            case "4": case "r": case "f": case "v":
+                Hands.fingerAnimation(".indiceL");
+                break;
+            case "5": case "t": case "g": case "b":
+                Hands.fingerAnimation(".indiceL");
+                break;
+            case "6": case "y": case "h": case "n":
+                Hands.fingerAnimation(".indiceR");
+                break;
+            case "7": case "u": case "j": case "m":
+                Hands.fingerAnimation(".indiceR");
+                break;
+            case "8": case "i": case "k": case ",":
+                Hands.fingerAnimation(".medioR");
+                break;
+            case "9": case "o": case "l": case ".":
+                Hands.fingerAnimation(".anularR");
+                break;
+            case "0": case "p": case "ñ":
+                Hands.fingerAnimation(".meniqueR");
+                break;
+        }
+    },
+
+    _inactiveAnimation(word){
+        switch (word) {
+            case "1": case "q": case "a": case "z": case "!":
+                Hands.fingerReturn(".meniqueL");
+                break;
+            case "2": case "w": case "s": case "x":
+                Hands.fingerReturn(".anularL");
+                break;
+            case "3": case "e": case "d": case "c":
+                Hands.fingerReturn(".medioL");
+                break;
+            case "4": case "r": case "f": case "v":
+                Hands.fingerReturn(".indiceL");
+                break;
+            case "5": case "t": case "g": case "b":
+                Hands.fingerReturn(".indiceL");
+                break;
+            case "6": case "y": case "h": case "n":
+                Hands.fingerReturn(".indiceR");
+                break;
+            case "7": case "u": case "j": case "m":
+                Hands.fingerReturn(".indiceR");
+                break;
+            case "8": case "i": case "k": case ",":
+                Hands.fingerReturn(".medioR");
+                break;
+            case "9": case "o": case "l": case ".":
+                Hands.fingerReturn(".anularR");
+                break;
+            case "0": case "p": case "ñ":
+                Hands.fingerReturn(".meniqueR");
                 break;
         }
     }
