15108a63477f840b11ea06448f1caa76416c228b
Key Cuevas Melgarejo <38257705+KeyCuevasMelgarejo@users.noreply.github.com>
2021-07-14 23:36:55 -0500

    fix bug
    fix event on btnLimpiar

--- js/events.js
+++ js/events.js
@@ -32,6 +32,9 @@ const Events = {
             e.preventDefault();
             Keyboard.properties.value="";
             writeInput.value="";
+
+            saveMemento(); // from keypress_undo.js
+            Text.removeAllText(placeHolderInput);
         }
     }
 };
