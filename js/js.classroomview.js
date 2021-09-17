bcd47d790fced32951d94a273a558d6ccf0cd5f5
Key Cuevas Melgarejo <38257705+KeyCuevasMelgarejo@users.noreply.github.com>
2021-09-17 00:38:17 -0500

    v1.3.0
    add modal button events

--- js/js.classroomview.js
+++ js/js.classroomview.js
@@ -31,27 +31,72 @@ const ClassRoomView = {
 
         optionsLayout.forEach(option => {
             const optionElement=document.createElement("button");
-            
+
             // Add attributes/classes
             optionElement.setAttribute("type", "button");
             optionElement.classList.add("btn-tiny");
 
             switch (option){
                 case "new":
-                    optionElement.title = "Nueva Práctica";
+                    optionElement.id="modal-option-new";
+                    optionElement.title = "Cambiar el texto para todos";
                     optionElement.innerHTML = '<i class="fas fa-sync-alt"></i>';
+                    optionElement.addEventListener("click", () => {
+                        // enable init button
+                        document.getElementById("modal-option-init").disabled = false;
+                        // disable buttons
+                        document.getElementById("modal-option-pause").disabled = true;
+                        document.getElementById("modal-option-stop").disabled = true;
+                        
+                        // remove class .target from each button
+                        this.elements.optionsContainer.querySelectorAll("button").forEach(button=>{
+                            button.classList.remove("target");
+                        })
+                        optionElement.classList.add("target");
+                    });
                     break;
                 case "init":
+                    optionElement.id="modal-option-init";
                     optionElement.title = "Iniciar";
                     optionElement.innerHTML = '<i class="fas fa-play-circle"></i>';
+                    optionElement.addEventListener("click", () => {
+                        // enable buttons
+                        document.getElementById("modal-option-pause").disabled = false;
+                        document.getElementById("modal-option-stop").disabled = false;
+
+                        this.elements.optionsContainer.querySelectorAll("button").forEach(button=>{
+                            button.classList.remove("target");
+                        })
+                        optionElement.classList.add("target");
+                    });
                     break;
                 case "pause":
+                    optionElement.id="modal-option-pause";
                     optionElement.title = "Pausar";
                     optionElement.innerHTML = '<i class="fas fa-pause-circle"></i>';
+                    optionElement.disabled = true;
+                    optionElement.addEventListener("click", () => {
+                        this.elements.optionsContainer.querySelectorAll("button").forEach(button=>{
+                            button.classList.remove("target");
+                        })
+                        optionElement.classList.add("target");
+                    });
                     break;
                 case "stop":
+                    optionElement.id="modal-option-stop";
                     optionElement.title = "Detener";
                     optionElement.innerHTML = '<i class="fas fa-stop-circle"></i>';
+                    optionElement.disabled = true;
+                    optionElement.addEventListener("click", () => {
+                        // disable buttons
+                        document.getElementById("modal-option-init").disabled = true;
+                        document.getElementById("modal-option-pause").disabled = true;
+
+                        this.elements.optionsContainer.querySelectorAll("button").forEach(button=>{
+                            button.classList.remove("target");
+                        })
+                        optionElement.classList.add("target");
+                    });
                     break;
             }
 
